de6362a35013e01f102e25068e0c2c50db17a01e
lgustavuh <luizgustavo_rs@outlook.com>
2020-03-29 18:03:20 -0300

    update

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -45,7 +45,7 @@ export default function Register(){
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos de sua ONG.</p>
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color = "#e02041" />
-                        Não Tenho Cadastro
+                        Voltar
                     </Link>
                 </section>
                 <form onSubmit={handleRegister}>
